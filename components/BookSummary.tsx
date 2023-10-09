9159279fa3ca3281fb90d137225052c289b90a31
Blakthorne <dlpolar38@gmail.com>
2023-10-09 00:12:50 -0400

    starting to display info in summary

--- components/BookSummary.tsx
+++ components/BookSummary.tsx
@@ -6,6 +6,28 @@ type SummaryComponent = {
     bookId: string
 }
 
+type MaterialData = {
+    materialForRepair: MaterialForRepair;
+    material: Material;
+    unitType: UnitType;
+    materialHeight?: MaterialHeight;
+    materialWidth?: MaterialWidth;
+}
+
+type RepairData = {
+    repair: Repair;
+    repairType: RepairType;
+    materialData: MaterialData[];
+}
+
+// Define a format to return to the client
+type SummaryData = {
+    book: Book;
+    owner: Owner;
+    bindingType: BindingType;
+    repairData: RepairData[];
+}
+
 /**
  * 
  * @param {string} book The specified book id
@@ -18,7 +40,7 @@ export default function BookSummary({ bookId }: SummaryComponent) {
     }
 
     // Retrieve the table requested by the parent component
-    const { data, error } = useSWR<object[], Error>('/api/summary/' + bookId)
+    const { data, error } = useSWR<SummaryData, Error>('/api/summary/' + bookId)
     if (error) console.log(error)
     if (!data) {
         return (
@@ -32,8 +54,24 @@ export default function BookSummary({ bookId }: SummaryComponent) {
     }
     
     return (
-        <div>
-            
+        <div className="flex flex-col min-h-screen">
+            <div className="font-sans text-slate-200 text-8xl text-center drop-shadow-lg tracking-wide mb-6">
+                {data.book.title}
+            </div>
+            <div className="font-sans text-slate-200 text-xl text-center drop-shadow-lg tracking-wide mb-14">
+                Owned by {data.owner.ownerName}
+            </div>
+            <div className="table border-collapse table-auto text-sm mt-16 mb-32 mx-auto">
+                <div className="table-row-group">
+                    <div className="table-cell border-b border-slate-600 font-medium p-4 pt-2 pb-2 text-left">Author</div>
+                    <div className="table-cell border-b border-slate-600 font-medium p-4 pt-2 pb-2 text-left">{data.book.author}</div>
+                </div>
+                <div className="table-row-group">
+                    <div className="table-cell border-b border-slate-600 font-medium p-4 pt-2 pb-2 text-left">Author</div>
+                    <div className="table-cell border-b border-slate-600 font-medium p-4 pt-2 pb-2 text-left">{data.book.author}</div>
+                </div>
+            </div>
         </div>
+        
     )
 }
