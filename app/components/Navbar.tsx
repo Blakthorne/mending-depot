63b0fed6a6568b14db479f665a3881c38bde4616
Blakthorne <dlpolar38@gmail.com>
2024-03-30 16:39:23 -0400

    add sign out button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Navbar({ setTheme, isDark }) {
     return (
@@ -22,6 +23,11 @@ export default function Navbar({ setTheme, isDark }) {
                     />
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                 </label>
+                <button className="btn"
+                        onClick={() => signOut()}
+                >
+                    Sign Out
+                </button>
             </div>
         </div>
     )
