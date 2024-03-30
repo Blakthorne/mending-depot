3d60d619eb41baefe84d95fa19fda10e87b0ec67
Blakthorne <dlpolar38@gmail.com>
2024-03-29 21:04:14 -0400

    create env in action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,16 +11,6 @@ NEXTAUTH_SECRET=$5
 GITHUB_CLIENT_ID=$6
 GITHUB_CLIENT_SECRET=$7
 
-SRC_DIR=mending-depot
-
-# Remove old source code if there
-if [ -d $SRC_DIR ];
-then
-    sudo rm -r $SRC_DIR
-fi
-
-# Clone new repo
-git clone git@github.com:Blakthorne/mending-depot.git
 
 # Go into newly cloned source code folder
 cd ~/mending-depot
@@ -29,16 +19,16 @@ cd ~/mending-depot
 sudo docker compose down
 
 # Create .env file with variables
-touch .env
-echo "DATABASE_URL=${DATABASE_URL}" >> .env
-echo "DATABASE_PASSWORD=${DATABASE_PASSWORD}" >> .env
-echo "URL=${URL}" >> .env
+# touch .env
+# echo "DATABASE_URL=${DATABASE_URL}" >> .env
+# echo "DATABASE_PASSWORD=${DATABASE_PASSWORD}" >> .env
+# echo "URL=${URL}" >> .env
 
-echo "NEXTAUTH_URL=${NEXTAUTH_URL}" >> .env
-echo "NEXTAUTH_SECRET=${NEXTAUTH_SECRET}" >> .env
+# echo "NEXTAUTH_URL=${NEXTAUTH_URL}" >> .env
+# echo "NEXTAUTH_SECRET=${NEXTAUTH_SECRET}" >> .env
 
-echo "GITHUB_CLIENT_ID=${GITHUB_CLIENT_ID}" >> .env
-echo "GITHUB_CLIENT_SECRET=${GITHUB_CLIENT_SECRET}" >> .env
+# echo "GITHUB_CLIENT_ID=${GITHUB_CLIENT_ID}" >> .env
+# echo "GITHUB_CLIENT_SECRET=${GITHUB_CLIENT_SECRET}" >> .env
 
 # Build new docker image
 sudo docker build --network host -t mending-depot-app:latest .
--- scripts/pull-new-source.sh
+++ scripts/pull-new-source.sh
@@ -7,3 +7,6 @@ if [ -d $SRC_DIR ];
 then
     sudo rm -r $SRC_DIR
 fi
+
+# Clone new repo
+git clone git@github.com:Blakthorne/mending-depot.git
