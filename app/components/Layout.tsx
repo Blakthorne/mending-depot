976b92fb055f0bd67caf96033ec77290a84c65f4
Blakthorne <dlpolar38@gmail.com>
2024-04-01 21:54:05 -0400

    remove local storage from theming

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -5,62 +5,12 @@ import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 
 export default function Layout({children}) {
-
-    // Store a boolean value for whether theme is dark or not
-    const [isDark, setIsDark] = useState(true)
-
-    // Store the theme name,
-    // Initialize to empty string
-    const [themeName, setThemeName] = useState("")
-
-    /**
-     * Set the theme name based on the isDark state value
-     */
-    const determineThemeName = (): void => {
-        if (isDark === true) {
-            setThemeName("dim")
-        }
-        else {
-            setThemeName("garden")
-        }
-    }
-
-    // Trigger only on first render of page;
-    // I can only perform these operations in a useEffect() since
-    // localStorage is only present on the client. I get a JSON error
-    // if I try to do it elsewhere
-    useEffect(() => {
-        
-        // Retrieve the isDark boolean from client localStorage
-        setIsDark(JSON.parse(localStorage.getItem("isDark")))
-
-        // Set the themeName based on the isDark boolean
-        determineThemeName()
-      }, [])
-
-    // Trigger when isDark is changed by the user in the Navbar component
-    useEffect(() => {
-
-        // Only set the isDark variable in localStorage if it's there already;
-        // This prevents it from double setting it on the first render as a side effect
-        // of the other useEffect() above
-        if (JSON.parse(localStorage.getItem("isDark")) !== null) {
-            localStorage.setItem('isDark', JSON.stringify(isDark));
-        }
-
-        // Set the themeName based on the isDark boolean
-        determineThemeName()
-      }, [isDark])
-
     return  (
-        <div data-theme={themeName} 
+        <div data-theme
              className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
-                <Navbar 
-                    setTheme={setIsDark}
-                    isDark={isDark}
-                />
+                <Navbar/>
                 <div className="pt-16 min-h-screen">
                     <SWRConfig
                         value = {{
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Navbar({ setTheme, isDark }) {
+export default function Navbar() {
     return (
         <div className="navbar sticky top-0 bg-base-100/90 border-b-2 border-base-300 backdrop-blur-md z-10">
             <div className="flex-none lg:hidden">
@@ -14,14 +14,14 @@ export default function Navbar({ setTheme, isDark }) {
             </div>
             <div className="flex-none">
                 <label className="flex cursor-pointer gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                     <input
                         type="checkbox"
-                        className="toggle"
-                        checked={isDark}
-                        onChange={() => setTheme(!isDark)}
+                        value="garden"
+                        className="toggle theme-controller"
                     />
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+                    
                 </label>
                 <button className="btn btn-neutral"
                         onClick={() => signIn()}
