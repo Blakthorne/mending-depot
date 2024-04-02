febb6fb38d85d621a9361263e6f5d9fbcf6c0719
Blakthorne <dlpolar38@gmail.com>
2024-04-01 21:16:47 -0400

    remove middleware for hot reloading in docker

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar({ setTheme, isDark }) {
                     />
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
                 </label>
-                <button className="btn"
+                <button className="btn btn-neutral"
                         onClick={() => signIn()}
                 >
                     Sign In
--- next.config.js
+++ next.config.js
@@ -5,28 +5,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  nextConfig, 
-  // webpackDevMiddleware: config => {
-  //   config.watchOptions = {
-  //     poll: 1000,
-  //     aggregateTimeout: 300,
-  //   }
-
-  //   return config
-  // },
-}
-
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
- 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      /* development only config options here */
-    }
-  }
- 
-  return {
-    /* config options for all phases except development here */
-  }
-}
+module.exports = nextConfig
